0b77b78a5e1a15d0dc315f53fee455382e0d2373
Katheeja-zuhaira23 <katheejakamal20@gmail.com>
2023-09-17 18:08:26 +0530

    uploading files

---
+++ script.js
@@ -0,0 +1,28 @@
+let family=[
+    {name:"katheeja",age:22,goal:"software developer",salary:400000},
+    {name:"zulfa",age:27,goal:"photographer",salary:15000},
+    {name:"kamal",age:28,goal:"marketting",salary:200000},
+    {name:"arifa",age:55,goal:"cooking expert",salary:10000}
+]
+
+
+// const age=details.filter((info)=>info.age<28);
+// console.log(age)
+
+
+// age.map((detail)=>{
+//     console.log(detail.name)
+// });
+
+// let goal=details.map((key)=>key.name)
+// console.log(goal)
+
+// details.push({name:"shajeea",age:2,goal:"IAS"})
+// console.log(details)
+
+// const tot_fam_salary= family.reduce(function(acc,value){
+//     return acc+value.salary
+// },0);
+
+// console.log(tot_fam_salary)
+
